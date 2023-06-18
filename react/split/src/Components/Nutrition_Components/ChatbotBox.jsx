1227026d26c1423270c5527dc9f7a908226a2b78
aariyur <ariyur.aditya@gmail.com>
2023-06-18 09:43:31 -0700

    UI Update and Query Updates

--- react/split/src/Components/Nutrition_Components/ChatbotBox.jsx
+++ react/split/src/Components/Nutrition_Components/ChatbotBox.jsx
@@ -1,38 +1,43 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import './ChatbotBox.css';
 import { FiSend } from 'react-icons/fi';
 
 const ChatbotBox = () => {
   const [emailContent, setEmailContent] = useState('');
-  const [response, setResponse] = useState('');
+  const [response, setResponse] = useState(``);
+  
+  const [textAreaHeight, setTextAreaHeight] = useState("auto");
 
-  const handleSubmit = useCallback(async (e) => {
+  useEffect(() => {
+    setTextAreaHeight(`${response.split("\n").length}`);
+  }, [response]);
+
+  const handleSubmit = useCallback((e) => {
     e.preventDefault();
 
-    try {
-      const url = new URL('http://localhost:5000/test');
-      url.searchParams.append('emailContent', emailContent);
+    const url = new URL('http://localhost:5000/emailgenerate');
+    url.searchParams.append('prompt', emailContent);
 
-      const response = await fetch(url, {
-        method: 'GET',
-        mode: 'cors',
-      });
-
-      if (!response.ok) {
-        throw new Error('Request failed');
-      }
-
-      const data = await response.json();
+    fetch(url, {
+      method: 'GET',
+      mode: 'cors',
+    })
+    .then((response) => response.json())
+    .then((data) => {
       console.log('Response:', data);
-      setResponse(data);
-    } catch (error) {
+      setResponse(data); 
+    })
+    .catch((error) => {
       console.error('Error:', error);
-    }
+    });
+
+    console.log('Sending request:', url); // Log the sent request
   }, [emailContent]);
 
   return (
     <div className="responseBox">
       <p className="chatbot-title">
+        <b>Chatbot</b>
       </p>
       <form onSubmit={handleSubmit}>
         <textarea
@@ -44,10 +49,19 @@ const ChatbotBox = () => {
           <FiSend />
         </button>
       </form>
+      
       {response && (
-        <div className="response">
-          <b>Assistant: </b>
-          {response}
+        <div>
+          <p className="chatbot-title"><b>Assistant</b></p>
+          <div className="response" style={{ textAlign: 'center', marginBottom: '20px', marginLeft: '-20px' }}>
+          <textarea
+            placeholder="Enter email content"
+            value={response}
+            style={{ resize: 'none', width: '90%', height: 'auto' }}
+            readOnly
+            rows={textAreaHeight}
+          />
+        </div>
         </div>
       )}
     </div>
