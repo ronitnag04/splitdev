8b89183096a90f33accd17506f932784a09cec3d
aariyur <ariyur.aditya@gmail.com>
2023-06-18 10:45:20 -0700

    Chatbot Fixed UI and Post Request

--- react/split/src/Components/Nutrition_Components/ChatbotBox.jsx
+++ react/split/src/Components/Nutrition_Components/ChatbotBox.jsx
@@ -1,11 +1,21 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback, useEffect, useRef } from 'react';
 import './ChatbotBox.css';
 import { FiSend } from 'react-icons/fi';
 
 const ChatbotBox = () => {
   const [emailContent, setEmailContent] = useState('');
   const [response, setResponse] = useState(``);
+  const [finalResponse, setFinalResponse] = useState('');  // New State
   
+  const textAreaRef = useRef(null);
+
+  useEffect(() => {
+    if(textAreaRef.current) {
+      textAreaRef.current.style.height = 'auto';
+      textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+    }
+  }, [response]);
+
   const handleSubmit = useCallback((e) => {
     e.preventDefault();
 
@@ -19,7 +29,7 @@ const ChatbotBox = () => {
     .then((response) => response.json())
     .then((data) => {
       console.log('Response:', data);
-      setResponse(data.response); 
+      setResponse(data); 
     })
     .catch((error) => {
       console.error('Error:', error);
@@ -27,6 +37,32 @@ const ChatbotBox = () => {
 
     console.log('Sending request:', url); // Log the sent request
   }, [emailContent]);
+
+  const handleSubmitFinal = useCallback((e) => {  // New Function
+    e.preventDefault();
+
+    // Insert your POST request here.
+    // I'm using a placeholder URL and dummy data
+    fetch('http://localhost:5000/emailsend', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        response: finalResponse
+      })
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Final Response:', data);
+      // Handle response here
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+    console.log('Sending final request:', finalResponse); // Log the sent request
+  }, [finalResponse]);
 
   return (
     <div className="responseBox">
@@ -48,12 +84,21 @@ const ChatbotBox = () => {
         <div>
           <p className="chatbot-title"><b>Assistant</b></p>
           <div className="response" style={{ textAlign: 'center', marginBottom: '20px', marginLeft: '-20px' }}>
-          <textarea
-            placeholder="Enter email content"
-            value={response}
-            style={{ resize: 'none', width: '90%', height: 'auto' }}
-            readOnly
-          />
+          <form onSubmit={handleSubmitFinal}>
+            <textarea
+              placeholder="Enter email content"
+              value={response}
+              style={{ resize: 'none', width: '90%', height: 'auto' }}
+              onChange={(e) => setFinalResponse(e.target.value)}  // Update finalResponse on change
+              readOnly
+              ref={textAreaRef}
+            />
+            <button type="submit">
+              <FiSend />
+            </button>
+          </form>
+          
+          
         </div>
         </div>
       )}
