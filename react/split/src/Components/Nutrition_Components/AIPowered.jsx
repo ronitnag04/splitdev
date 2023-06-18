e76c5c2df308aa70cb974ea67f2642ea85145f32
aariyur <ariyur.aditya@gmail.com>
2023-06-17 19:09:42 -0700

    Nutrition Page Done v1

---
+++ react/split/src/Components/Nutrition_Components/AIPowered.jsx
@@ -0,0 +1,14 @@
+// AIPowered.jsx
+import React from 'react';
+import './AIPowered.css';
+
+const AIPowered = () => {
+  return (
+    <div className="ai-powered">
+      <h1>AI</h1>
+      <p>This chatbot is powered by advanced AI technologies. It processes your input and generates responses in real time.</p>
+    </div>
+  );
+};
+
+export default AIPowered;
